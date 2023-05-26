b61453909227afe9290385cd1aaf1cb854fc68a3
Ariffl <119127219+Ariffl@users.noreply.github.com>
2023-05-27 00:23:48 +0300

    2 files modified

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,29 @@
 import Head from 'next/head';
+import { useState } from 'react';
 
 export default function Home() {
-  return <div></div>;
+  const [avatar, setAvatar] = useState();
+
+  const handleAvatar = (e) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.addEventListener('load', function () {
+      setAvatar(this.result);
+    });
+    reader.readAsDataURL(file);
+  };
+  return (
+    <div>
+      <div>
+        <label htmlFor="changeAvatar">Profil resmini değiştirmek için tıklayın..</label>
+        <input type="file" id="changeAvatar" onChange={handleAvatar}></input>
+      </div>
+      {(avatar && (
+        <div className="image-box">
+          <img src={avatar} alt="Image" />
+        </div>
+      )) ||
+        null}
+    </div>
+  );
 }
